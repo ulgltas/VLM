9868d7bc810e12a2b934c2c41bdb18f25d50d594
Mariano Sánchez Martínez <m.sanchezmartinez@student.uliege.be>
2019-10-28 12:32:57 +0100

    Moving utilities to own module

--- pythonVLM/__init__.py
+++ pythonVLM/__init__.py
@@ -1,3 +1,6 @@
+import VLM_utilities
+VLM_utilities.add_path("geoGen")
+VLM_utilities.add_path("VLM/bin")
 import VLM_inputs
 import VLM_testing
 import VLM_driver
--- run.py
+++ run.py
@@ -2,37 +2,10 @@
 # -*- coding: utf8 -*-
 # VLM run script
 
-def create_workspace(name, origin):
-    import os
-    from distutils import dir_util
-
-    vlm_dir = os.path.abspath(os.path.split(__file__)[0])
-    workspace = os.path.join(vlm_dir, "workspace", name)
-    origin_models = os.path.join(vlm_dir, origin, "models")
-    models = os.path.join(workspace, "models")
-    if not os.path.exists(models):
-        os.makedirs(models)
-    if os.path.exists(origin_models):
-        dir_util.copy_tree(origin_models, models)
-    os.chdir(workspace)
-    return workspace
-
-def add_path(name):
-    import os, sys
-    vlm_dir = os.path.abspath(os.path.split(__file__)[0])
-    path = os.path.join(vlm_dir, "..", name)
-    if os.path.isdir(path):
-         print("INFO: adding {} to PYTHONPATH".format(path))
-         sys.path.append(path)
-    else:
-        print("INFO: {} not found!".format(path))
+import argparse, os
+import pythonVLM.VLM_utilities as utils
 
 def main():
-    add_path("geoGen")
-    add_path("VLM/bin")
-
-    import argparse, os
-
     parser = argparse.ArgumentParser()
     parser.add_argument("file", nargs=1, help="Python input file")
     parser.add_argument("--nogui", action="store_true", help="Disable GUI", default=False)
@@ -46,9 +19,9 @@ def main():
     name = names[-2]
     origin = os.path.split(args.file[0])[0]
 
-    add_path(os.path.join("VLM",origin))
+    utils.add_path(os.path.join("VLM",origin))
 
-    workspace = create_workspace(name, origin)
+    workspace = utils.create_workspace(name, origin)
     print("Workspace: {}".format(workspace))
 
     
